33093591b37394512721b96252091982cd049e70
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2024-07-06 11:15:29 +0530

    Almost the project is ready

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,47 +1,123 @@
-import json from "./sidebar.json";
+import { useState } from "react";
 import "./index.css";
-// import { useState } from "react";
+function App() {
+  const config = [
+    {
+      label: "Parent1",
+      id: 1,
+      children: [
+        {
+          label: "Child p1.c1",
+          id: 2,
+        },
+        {
+          label: "Child p1.c2",
+          id: 3,
+        },
+        {
+          label: "Childp1.c3",
+          id: 4,
+        },
+      ],
+    },
+    {
+      label: "Parent2",
+      id: 5,
+      children: [
+        {
+          label: "Childp2.c1",
+          id: 6,
+        },
+      ],
+    },
+  ];
 
-// interface departmentProps {
-//   department: string;
-//   subDepartment: string[];
-// }
+  const [checkBoxItems, setCheckBoxItem] = useState({});
 
-const Sidebar = () => {
-  // const [selected, setSelected] = useState<departmentProps[]>();
+  // @ts-ignore
+  const handleChildChange = (event, parentId, childId) => {
+    // @ts-ignore
+    const isChildChecked = checkBoxItems[childId] === true;
+    setCheckBoxItem({
+      ...checkBoxItems,
+      [childId]: isChildChecked ? false : true,
+      [parentId]: !isChildChecked,
+    });
+  };
+  // @ts-ignore
+  const ChildBox = ({ child, parentId, childId }) => {
+    return (
+      <>
+        <div style={{ display: "flex", gap: "0.2rem" }}>
+          <input
+            type="checkbox"
+            // @ts-ignore
+            checked={checkBoxItems[childId]}
+            onChange={(event) => handleChildChange(event, parentId, childId)}
+          />
+          <label>{child.label}</label>
+        </div>
+        <br />
+        {child.children &&
+          // @ts-ignore
+          child.children.map((subItem) => (
+            // @ts-ignore
+            <ChildBox key={subItem.id} child={subItem} /> // Recursive call
+          ))}
+      </>
+    );
+  };
+  // @ts-ignore
+  const handleClick = (event, parentId: number) => {
+    const isChecked = event.target.checked;
+    const updatedItems = { ...checkBoxItems, [parentId]: isChecked };
+
+    // Check/uncheck all child checkboxes related to this parent
+    const parentItem = config.find((item) => item.id === parentId);
+    // @ts-ignore
+    if (parentItem.children) {
+      // @ts-ignore
+      parentItem.children.forEach((child) => {
+        // @ts-ignore
+        updatedItems[child.id] = isChecked;
+      });
+    }
+
+    setCheckBoxItem(updatedItems);
+  };
+
   return (
-    <div>
-      {json.map((d1, i1) => {
-        return (
-          <details key={i1} id="sidebar_details">
-            <summary style={{ display: "flex", gap: "0.2rem" }}>
-              <input type="checkbox" name={d1.department} id="" />
-              <span>{d1.department}</span>
-              <span>({d1.sub_departments.length})</span>
-            </summary>
-            {d1.sub_departments.map((d2, i2) => {
-              return (
-                <div
-                  className=""
-                  key={i2}
-                  style={{ display: "flex", gap: "0.2rem" }}
-                >
-                  <input type="checkbox" name={d2} id="" />
-                  <span>{d2}</span>
-                </div>
-              );
-            })}
-          </details>
-        );
-      })}
+    <div
+      className="App"
+      style={{ padding: "20px", backgroundColor: "#f5f5f5" }}
+    >
+      {config.map((item) => (
+        <details key={item.id} style={{ marginBottom: "10px" }}>
+          <summary style={{ display: "flex", gap: "0.2rem" }}>
+            <input
+              type="checkbox"
+              // @ts-ignore
+              checked={checkBoxItems[item.id]}
+              onChange={(e) => handleClick(e, item.id)}
+            />
+            <label>{item.label}</label>
+          </summary>
+
+          <div style={{ marginLeft: "20px" }}>
+            {item.children &&
+              item.children.map((subItem) => (
+                <ChildBox
+                  key={subItem.id}
+                  child={subItem}
+                  parentId={item.id}
+                  childId={subItem.id}
+                />
+              ))}
+          </div>
+        </details>
+      ))}
     </div>
   );
-};
+}
 
-export default Sidebar;
-
-// const Sidebar = () => {
-//   return <div>Sidebar</div>;
-// };
-
-// export default Sidebar;
+export default App;
